1126ee4000acf12b8183a4ff05161454ffc500d5
Alejandro Capparelli <alejandro.capparelli@gmail.com>
2020-06-09 21:01:53 +0200

    Added meta tag description

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -11,6 +11,7 @@ const Meta = ({ children }: MyProps): JSX.Element => (
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
+      <meta name="description" content="Platform where reviewers can find free books to review on their literary blogs, booktube or bookstagram. Authors can contact literary reviewers to promote their books." />
       <link rel="shortcut icon" href="/static/favicon.jpg" />
       <title>Reseñan Sancho</title>
       <link href="https://fonts.googleapis.com/css?family=Rambla&display=swap" rel="stylesheet" />
